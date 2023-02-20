32c3ed6c95ad804efc90c706f81ec3ab74f6858b
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2023-02-20 16:59:24 -0300

    feat: configuração JWT

---
+++ ECommerce/src/Services/NSE.Autenticacao.API/Extensions/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Autenticacao.API.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; } = string.Empty;
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; } = string.Empty;
+        public string ValidoEm { get; set; } = string.Empty;
+    }
+}
--- ECommerce/src/Services/NSE.Autenticacao.API/Program.cs
+++ ECommerce/src/Services/NSE.Autenticacao.API/Program.cs
@@ -1,12 +1,15 @@
+using System.Text;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 using NSE.Autenticacao.API.Data;
+using NSE.Autenticacao.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -15,6 +18,30 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
        //.AddErrorDescriber<IdentityMensagensPortugues>()
        .AddEntityFrameworkStores<ApplicationDbContext>()
        .AddDefaultTokenProviders();
+
+var appSettingsSection = builder.Configuration.GetSection(nameof(AppSettings));
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+var appSettings = appSettingsSection.Get<AppSettings>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(bearerOptions =>
+{
+    bearerOptions.RequireHttpsMetadata = true;
+    bearerOptions.SaveToken = true;
+    bearerOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(appSettings.Secret)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = appSettings.ValidoEm,
+        ValidIssuer = appSettings.Emissor,
+    };
+});
 
 builder.Services.AddSwaggerGen(options =>
 {
