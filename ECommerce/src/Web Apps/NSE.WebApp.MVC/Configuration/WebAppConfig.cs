fd2900eb1e6ec23aa3be024c23524484de2cb914
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2023-02-21 14:00:54 -0300

    feat: criação classes de configurações da web app

---
+++ ECommerce/src/Web Apps/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,34 @@
+﻿namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static IServiceCollection AddWebAppConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            return services;
+        }
+
+        public static WebApplication UseWebAppConfiguration(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            return app;
+        }
+    }
+}
