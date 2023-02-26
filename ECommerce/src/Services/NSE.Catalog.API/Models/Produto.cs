2ba091cd5e13e7f07fddbf0b8d543127cd2b8742
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2023-02-26 11:14:32 -0300

    feat: criação da entidade produto

---
+++ ECommerce/src/Services/NSE.Catalog.API/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Catalog.API.Models
+{
+    public class Produto : Entity
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public bool Ativo { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Imagem { get; set; } = string.Empty;
+        public int QuantidadeEstoque { get; set; }
+    }
+}
