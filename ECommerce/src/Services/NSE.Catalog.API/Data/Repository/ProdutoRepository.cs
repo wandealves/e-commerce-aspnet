b67dbe307925acdd01503c607beb4e2eac742d24
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2023-02-26 11:54:58 -0300

    feat: criação repositório e mapeamento de produto

---
+++ ECommerce/src/Services/NSE.Catalog.API/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using NSE.Catalog.API.Models;
+using NSE.Core.Data;
+
+namespace NSE.Catalog.API.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _catalogoContext;
+        public IUnitOfWork unitOfWork => _catalogoContext;
+
+        public ProdutoRepository(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
+
+        public async Task<Produto?> ObterPorId(Guid id)
+        {
+            return await _catalogoContext.Produtos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Produto?>> ObterTodos()
+        {
+            return await _catalogoContext.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            _catalogoContext.Produtos.Add(produto);
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _catalogoContext.Produtos.Update(produto);
+        }
+
+        public void Dispose()
+        {
+            _catalogoContext?.Dispose();
+        }
+
+    }
+}
