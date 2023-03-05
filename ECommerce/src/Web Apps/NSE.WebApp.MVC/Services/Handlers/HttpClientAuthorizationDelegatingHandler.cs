61e179eefcd3432a16ea442c6ac0155a42ecefc2
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2023-03-05 11:12:36 -0300

    feat: criação delegating handler

---
+++ ECommerce/src/Web Apps/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+
+using NSE.WebApp.MVC.Extensions;
+
+namespace NSE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user?.ObterHttpContext()?.Request.Headers["Authorization"];
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string> { authorizationHeader });
+            }
+            var token = _user?.ObterUserToken();
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
