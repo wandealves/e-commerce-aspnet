b1669c5b90e3a1780d4bab17ae9063221c2b22b8
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2023-03-05 10:51:26 -0300

    feat: criação tela, controle e serviço para consumir a API de catalogo

--- ECommerce/src/Web Apps/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ ECommerce/src/Web Apps/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -48,7 +48,7 @@ namespace NSE.WebApp.MVC.Configuration
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Catalogo}/{action=Index}/{id?}");
 
             return app;
         }
---
+++ ECommerce/src/Web Apps/NSE.WebApp.MVC/Services/CatalogoService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Models;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class CatalogoService : Service, ICatalogoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogoService(HttpClient httpClient, IOptions<AppSettings> settings)
+        {
+            httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
+            _httpClient = httpClient;
+        }
+
+        public async Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+            TratarErrosResponse(response);
+            var result = await DeserializarObjetoResponse<ProdutoViewModel>(response);
+            return result ?? new ProdutoViewModel();
+        }
+
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            var response = await _httpClient.GetAsync("/catalogo/produtos");
+            TratarErrosResponse(response);
+            var result = await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
+            return result ?? Enumerable.Empty<ProdutoViewModel>();
+        }
+    }
+}
